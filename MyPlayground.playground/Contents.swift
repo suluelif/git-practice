463d3f2f6bff80f6d74d97bc20c1ba876a16000f
Apple <apple@Apples-MacBook-Pro-10.local>
2019-06-17 14:43:37 -0400

    added comment

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,55 @@
+import UIKit
+//var name = Juliya
+//print("Hello, /(name)")
+
+//let first = "Karlie"
+//let last = "Kloss"
+//var name = "\(first) \(last)"
+
+//10/3
+//3+3
+//10.0/3.0
+//var bigNumber = 15
+//var smallNumber = 2.0
+//Double (bigNumber)/smallNumber
+//
+//"karlie" == "karlie"
+//"karlie" == " karlie"
+//"Karlie" == "karlie"
+//var luckyNum = 7
+//luckyNum < 10
+//luckyNum == 7
+//7 == "7"
+//var favoriteFood = "subway"
+//if favoriteFood != "subway" {
+//    print ("You are not right")
+//
+//}
+//else if favoriteFood == "subway" {
+//    print ("You are right!")
+//
+//}
+//favoriteFood == "chipotle"
+
+//var hasAccount = false
+//hasAccount == true
+//if hasAccount == false {
+//    print ("let's get some info to create an account for you.")
+//}
+//else if hasAccount != false {
+//    print ("Please log in!")
+//}
+//hasAccount == true
+
+var studentAge = 18
+if studentAge <= 12 {
+    print (" You go to elementary school.")
+}
+else if studentAge <= 15 {
+    print ("You are a middle schooler")
+}
+else if studentAge >= 15 {
+    print ("You are a high schooler")
+}
+else {("that is not a grade") }
+
